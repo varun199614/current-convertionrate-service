012339adb25c5659b63232e1ae11b98cb9666de6
varun199614 <58078466+varun199614@users.noreply.github.com>
2020-01-10 16:20:33 +0530

    sql data update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-insert into EXCHANGE_RATES values (1001,70,'USA','0','IND');  
+insert into EXCHANGE_RATES values (1001,69.5,'USA','0','IND');  
 insert into EXCHANGE_RATES values (1002,0.014,'IND','0','USA');  
 insert into EXCHANGE_RATES values (1003,45,'NZ','0','IND');   
+insert into EXCHANGE_RATES values (1001,69.5,'USA','0','IND');  
